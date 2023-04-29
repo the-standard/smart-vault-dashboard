086345e1f7b67c918333e62f90b59bf62fd5235d
muratcan-yuksel <muratcanyukselpro@gmail.com>
2023-04-29 20:53:14 +0300

    starters

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import {
+  EthereumClient,
+  w3mConnectors,
+  w3mProvider,
+} from "@web3modal/ethereum";
+import { Web3Modal } from "@web3modal/react";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { mainnet, polygon } from "wagmi/chains";
+import HomePage from "./pages/HomePage.tsx";
+
+const chains = [mainnet, polygon];
+const projectId = import.meta.env.VITE_WEB3MODAL_PROJECT_ID;
+
+const { provider } = configureChains(chains, [w3mProvider({ projectId })]);
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors: w3mConnectors({ projectId, version: 1, chains }),
+  provider,
+});
+const ethereumClient = new EthereumClient(wagmiClient, chains);
+
+function App() {
+  return (
+    <>
+      <WagmiConfig client={wagmiClient}>
+        <HomePage />
+      </WagmiConfig>
+
+      <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
+    </>
+  );
+}
+
+export default App;
