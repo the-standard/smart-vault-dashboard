cf81f7d832d5413ac91aea49c4267764d23daad1
muratcan-yuksel <muratcanyukselpro@gmail.com>
2023-05-13 20:04:06 +0300

    FINALLY, GOT THE SIGNATURE WORKING

--- src/components/modal/StepTwo.tsx
+++ src/components/modal/StepTwo.tsx
@@ -4,11 +4,11 @@ import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import { Button } from "@mui/material";
-// import { OpenSeaSDK, Network } from "opensea-js";
 import { ethers } from "ethers";
 import { Seaport } from "@opensea/seaport-js";
 // import { ItemType } from "@opensea/seaport-js/lib/constants";
 // import axios from "axios";
+import { useAccount } from "wagmi";
 
 interface StepProps {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -31,16 +31,180 @@ const StepTwo: React.FC<StepProps> = ({
   // }
   console.log(onDataFromChild);
 
+  const { address, isConnecting, isDisconnected } = useAccount();
+
   const provider = new ethers.providers.Web3Provider(window.ethereum);
   const signer = provider.getSigner();
   console.log(signer);
 
-  const seaport = new Seaport(signer);
-  //console.log(seaport);
+  const seaport = new Seaport(signer, { seaportVersion: "1.4" }); //console.log(seaport);
+  const [myPrices, setMyPrices] = React.useState<object[]>([]);
+
+  function calculatePrices(listing_price: number, royalty: number) {
+    try {
+      const opensea_fee = Number((listing_price * 0.025).toFixed(18));
+      const royalty_fee = Number(
+        (listing_price / (1 / (royalty / 100 / 100))).toFixed(18)
+      ); // 100 cause percent comes in *100
+      const listing_profit = Number(
+        (listing_price - opensea_fee - royalty_fee).toFixed(18)
+      );
+
+      console.log(listing_profit);
+      console.log(royalty_fee);
+      console.log(opensea_fee);
+
+      // setMyPrices([
+      //   {
+      //     listing_profit: ethers.utils
+      //       .parseEther(String(listing_profit))
+      //       .toString(),
+      //     royalty_fee: ethers.utils.parseEther(String(royalty_fee)).toString(),
+      //     opensea_fee: ethers.utils.parseEther(String(opensea_fee)).toString(),
+      //   },
+      // ]);
+
+      return {
+        listing_profit: ethers.utils
+          .parseEther(String(listing_profit))
+          .toString(),
+        royalty_fee: ethers.utils.parseEther(String(royalty_fee)).toString(),
+        opensea_fee: ethers.utils.parseEther(String(opensea_fee)).toString(),
+      };
+    } catch (error) {
+      console.log(error);
+      return -1;
+    }
+  }
+
+  const prices = calculatePrices(1, 1);
+
+  const new_parameters = {
+    offerer: "0x600044FE9A152C27f337BbB23803dC6A68E3eFB0",
+    offer: [
+      {
+        itemType: 2,
+        token: "0xE7903CE9191D6e13fbF4dD7b183F6d0d961e778D",
+        identifierOrCriteria: 2,
+        startAmount: "1",
+        endAmount: "1",
+      },
+    ],
+    consideration: [
+      {
+        itemType: 0,
+        token: "0x0000000000000000000000000000000000000000",
+        identifierOrCriteria: "0",
+        startAmount: "974900000000000000",
+        endAmount: "974900000000000000",
+        recipient: "0x600044FE9A152C27f337BbB23803dC6A68E3eFB0",
+      },
+      {
+        itemType: 0,
+        token: "0x0000000000000000000000000000000000000000",
+        identifierOrCriteria: "0",
+        startAmount: "25000000000000000",
+        endAmount: "25000000000000000",
+        recipient: "0x8De9C5A032463C561423387a9648c5C7BCC5BC90",
+      },
+      {
+        itemType: 0,
+        token: "0x0000000000000000000000000000000000000000",
+        identifierOrCriteria: "0",
+        startAmount: "100000000000000",
+        endAmount: "100000000000000",
+        recipient: "0x600044FE9A152C27f337BbB23803dC6A68E3eFB0",
+      },
+    ],
+    startTime: ethers.BigNumber.from(
+      Math.floor(new Date().getTime() / 1000).toString()
+    ),
+    endTime: ethers.BigNumber.from(
+      Math.floor(new Date().getTime() / 1000 + 60 * 60 * 11 * 24 * 7).toString()
+    ),
+    orderType: 2,
+    zone: "0x004C00500000aD104D7DBd00e3ae0A5C00560C00",
+    zoneHash:
+      "0x0000000000000000000000000000000000000000000000000000000000000000",
+    salt: "30158852458975423",
+    conduitKey:
+      "0x0000007b02230091a7ed01230072f7006a004d60a8d4e71d599b8104250f0000",
+    totalOriginalConsiderationItems: ethers.BigNumber.from("3"),
+  };
 
   const handleListing = async () => {
-    console.log(seaport);
-    console.log(signer);
+    console.log(seaport.signOrder);
+    const buyer = address;
+    console.log(buyer);
+    console.log(provider);
+    console.log(address);
+    calculatePrices(1, 1);
+    console.log(prices);
+
+    const counter = 0;
+    const signature = await seaport.signOrder(
+      {
+        counter: 0,
+        offerer: "0x600044FE9A152C27f337BbB23803dC6A68E3eFB0",
+        offer: [
+          {
+            itemType: 2,
+            token: "0xE7903CE9191D6e13fbF4dD7b183F6d0d961e778D",
+            identifierOrCriteria: "2",
+            startAmount: "1",
+            endAmount: "1",
+          },
+        ],
+        consideration: [
+          {
+            itemType: 0,
+            token: "0x0000000000000000000000000000000000000000",
+            identifierOrCriteria: "0",
+            startAmount: "974900000000000000",
+            endAmount: "974900000000000000",
+            recipient: "0x600044FE9A152C27f337BbB23803dC6A68E3eFB0",
+          },
+          {
+            itemType: 0,
+            token: "0x0000000000000000000000000000000000000000",
+            identifierOrCriteria: "0",
+            startAmount: "25000000000000000",
+            endAmount: "25000000000000000",
+            recipient: "0x8De9C5A032463C561423387a9648c5C7BCC5BC90",
+          },
+          {
+            itemType: 0,
+            token: "0x0000000000000000000000000000000000000000",
+            identifierOrCriteria: "0",
+            startAmount: "100000000000000",
+            endAmount: "100000000000000",
+            recipient: "0x600044FE9A152C27f337BbB23803dC6A68E3eFB0",
+          },
+        ],
+        startTime: ethers.BigNumber.from(
+          Math.floor(new Date().getTime() / 1000).toString()
+        ),
+        endTime: ethers.BigNumber.from(
+          Math.floor(
+            new Date().getTime() / 1000 + 60 * 60 * 11 * 24 * 7
+          ).toString()
+        ),
+        orderType: 2,
+        zone: "0x004C00500000aD104D7DBd00e3ae0A5C00560C00",
+        zoneHash:
+          "0x0000000000000000000000000000000000000000000000000000000000000000",
+        salt: "30158852458975423",
+        conduitKey:
+          "0x0000007b02230091a7ed01230072f7006a004d60a8d4e71d599b8104250f0000",
+        totalOriginalConsiderationItems: ethers.BigNumber.from("3"),
+      },
+      "0xE7903CE9191D6e13fbF4dD7b183F6d0d961e778D"
+    );
+    console.log(signature);
+    return {
+      parameters: new_parameters,
+      signature: signature,
+    };
   };
   return (
     <Box sx={{ color: "white" }}>
