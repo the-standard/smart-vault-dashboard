508dcb3f13a759924c1b41f82fe0538c72b5c74b
muratcan-yuksel <muratcanyukselpro@gmail.com>
2023-05-17 08:01:00 +0300

    add empty collateral card

---
+++ src/components/collateral/EmptyCard.tsx
@@ -0,0 +1,112 @@
+import { Box, Typography } from "@mui/material";
+import closeIcon from "../../assets/closeIcon.png";
+
+const EmptyCard = () => {
+  return (
+    <Box
+      sx={{
+        borderRadius: "16px",
+        boxShadow: "0 4px 30px rgba(0, 0, 0, 0.1)",
+        backdropFilter: "blur(5px)",
+        border: "1px solid rgba(255, 255, 255, 0.3)",
+        background: "rgba(255, 255, 255, 0.07)",
+        width: "40%",
+        height: "10rem",
+        padding: "1rem",
+        marginTop: "0.5rem",
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "flex-start",
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <Box
+          sx={{
+            color: "#00FFF0",
+            border: "1px solid #8E9BAE",
+            borderRadius: "50%",
+            width: "3.5rem",
+            height: "3.5rem",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            cursor: "pointer",
+            animation: "glowing 2s infinite",
+            boxShadow: () => `0 0 5px #00FFF0`,
+            "@keyframes glowing": {
+              "0%": {
+                boxShadow: () => `0 0 5px #00FFF0`,
+              },
+              "50%": {
+                boxShadow: () => `0 0 20px #00FFF0`,
+              },
+              "100%": {
+                boxShadow: () => `0 0 5px #00FFF0`,
+              },
+            },
+          }}
+        >
+          <Typography variant="h4">+</Typography>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Box
+            sx={{
+              borderBottom: "1px solid #8E9BAE",
+              width: "2rem",
+              marginRight: "1rem",
+            }}
+          ></Box>
+          <Box>0.0000</Box>
+        </Box>
+        <Box
+          sx={{
+            color: "white",
+            fontSize: "1.3rem",
+          }}
+        >
+          €0.00
+        </Box>
+      </Box>
+      {/* right */}
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          borderBottom: "1px solid #8E9BAE",
+          justifyContent: "space-around",
+          alignItems: "center",
+          width: "70%",
+          height: "3rem",
+        }}
+      >
+        <Box>$0.00</Box>
+        <Box
+          sx={{
+            width: "2rem",
+            borderBottom: "1px solid #8E9BAE",
+          }}
+        ></Box>
+        <Box>0.00%</Box>
+        <img
+          src={closeIcon}
+          alt="close"
+          style={{ cursor: "pointer", alignSelf: "flex-start" }}
+        />
+      </Box>
+    </Box>
+  );
+};
+
+export default EmptyCard;
--- src/pages/Collateral.tsx
+++ src/pages/Collateral.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useVaultIdStore } from "../store/Store";
 import { Box } from "@mui/material";
+import QRicon from "../assets/qricon.png";
+import EmptyCard from "../components/collateral/EmptyCard";
 
 const Collateral = () => {
   const { vault, getVaultID } = useVaultIdStore();
@@ -16,6 +18,7 @@ const Collateral = () => {
   return (
     <Box
       sx={{
+        color: "#8E9BAE",
         margin: "3% 9%",
         padding: "3%",
         // marginTop: "50px",
@@ -72,9 +75,116 @@ const Collateral = () => {
             Debt
           </Box>
         </Box>
+        {/* right side of the upper column */}
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "flex-start",
+          }}
+        >
+          <Box
+            sx={{
+              background: " rgba(18, 18, 18, 0.5)",
+              boxShadow:
+                " 0px 1.24986px 1.24986px rgba(255, 255, 255, 0.5), inset 0px 1.24986px 0px rgba(0, 0, 0, 0.25)",
+              borderRadius: "6.24932px",
+              cursor: "pointer",
+              marginLeft: "2rem",
+            }}
+          >
+            <Box
+              sx={{
+                margin: "2px",
+                padding: "5px",
+              }}
+              className={activeElement === 3 ? "glowingCard" : ""}
+              onClick={() => handleClick(3)}
+            >
+              + Add Token
+            </Box>
+          </Box>{" "}
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "flex-end",
+            }}
+          >
+            <Box
+              sx={{
+                background: " rgba(18, 18, 18, 0.5)",
+                boxShadow:
+                  " 0px 1.24986px 1.24986px rgba(255, 255, 255, 0.5), inset 0px 1.24986px 0px rgba(0, 0, 0, 0.25)",
+                borderRadius: "6.24932px",
+                cursor: "pointer",
+                marginLeft: "2rem",
+              }}
+            >
+              <Box
+                sx={{
+                  margin: "2px",
+                  padding: "5px",
+                }}
+                className={activeElement === 4 ? "glowingCard" : ""}
+                onClick={() => handleClick(4)}
+              >
+                List of Open Smart Vaults
+              </Box>
+            </Box>
+            <Box
+              sx={{
+                marginTop: "1rem",
+              }}
+            >
+              sEuro Smart Vault #0
+            </Box>
+          </Box>
+        </Box>
       </Box>
       {/*  column 2 */}
-      <Box></Box>
+      <Box>
+        {/* divide into 2 rows */}
+        {/*  row 1 */}
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "flex-start",
+          }}
+        >
+          <Box
+            sx={{
+              background: " rgba(18, 18, 18, 0.5)",
+              boxShadow:
+                " 0px 1.24986px 1.24986px rgba(255, 255, 255, 0.5), inset 0px 1.24986px 0px rgba(0, 0, 0, 0.25)",
+              borderRadius: "6.24932px",
+              cursor: "pointer",
+              width: "40%",
+            }}
+          >
+            <Box
+              sx={{
+                margin: "2px",
+                padding: "5px",
+              }}
+              className="glowingCard"
+            >
+              + Deposit Collateral{" "}
+              <img
+                style={{
+                  marginLeft: "1.5rem",
+                }}
+                src={QRicon}
+                alt="qricon"
+              />
+            </Box>
+          </Box>
+          <EmptyCard />
+        </Box>
+        {/*  row 2 */}
+      </Box>
     </Box>
   );
 };
