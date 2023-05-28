0246af3389d6dcf3c59fd60e41b3b08602c026ad
muratcan-yuksel <muratcanyukselpro@gmail.com>
2023-05-28 21:21:19 +0300

    conditionally render line cahrt color

--- src/components/collateral/LineChart.tsx
+++ src/components/collateral/LineChart.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
 import { VictoryChart, VictoryLine } from "victory";
 import priceFeed from "../../feed/priceFeed";
 
@@ -7,11 +7,29 @@ interface LineChartProps {
 }
 
 const LineChart: React.FC<LineChartProps> = ({ data }) => {
+  const [lineColor, setLineColor] = useState("green");
+  console.log(data[data.length - 1].x);
+
+  const renderColor = () => {
+    //if the last element is greater than the second to last element, set lineColor to green
+    if (Number(data[data.length - 1].x) > Number(data[data.length - 2].x)) {
+      setLineColor("green");
+    } else if (
+      Number(data[data.length - 1].x) < Number(data[data.length - 2].x)
+    ) {
+      setLineColor("red");
+    }
+  };
+
+  useEffect(() => {
+    renderColor();
+  }, []);
+
   return (
     <VictoryLine
       data={data}
       style={{
-        data: { stroke: "red" },
+        data: { stroke: lineColor },
       }}
     />
   );
