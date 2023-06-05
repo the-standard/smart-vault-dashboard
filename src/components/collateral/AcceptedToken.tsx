9df299bf61f75fff35e1a6ecfd1e474f76b510c6
muratcan-yuksel <muratcanyukselpro@gmail.com>
2023-06-05 10:19:58 +0300

    redeployment2

--- src/components/collateral/AcceptedToken.tsx
+++ src/components/collateral/AcceptedToken.tsx
@@ -104,7 +104,6 @@ const AcceptedToken: React.FC<AcceptedTokenProps> = ({ amount, symbol }) => {
             minWidth: { xs: "100%", lg: "400px" },
             width: "auto",
             height: { xs: "100px", sm: "150px", md: "200px" },
-            // border: "1px solid #8E9BAE",
           }}
         >
           {renderLineChart()} {/* <LineChart /> */}
