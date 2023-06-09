c7181c9907b21117070c916ba05f856271836bae
muratcan-yuksel <muratcanyukselpro@gmail.com>
2023-06-09 19:23:17 +0300

    add progress values but needs to be xs responsive

--- src/components/chart/index.tsx
+++ src/components/chart/index.tsx
@@ -4,34 +4,15 @@ import { Box, Typography } from "@mui/material";
 import ProgressBar from "../ProgressBar";
 import { useVaultStore } from "../../store/Store";
 import { ethers } from "ethers";
-import { formatUnits } from "viem";
-
-const dummyData = [
-  {
-    title: "Debt Minted",
-    value: 100,
-    currency: "sEURO",
-  },
-  {
-    title: "Vault Collateral Value",
-    value: 100,
-    currency: "sEURO",
-  },
-  {
-    title: "Vault Collateral Value Liquidation Trigger",
-    value: 100,
-    currency: "sEURO",
-  },
-  {
-    title: "You can borrow up to:",
-    value: 100,
-    currency: "sEURO",
-  },
-];
+import { formatEther, formatUnits, fromHex } from "viem";
+import { useEffect, useState } from "react";
 
 const Index = () => {
-  const { getVaultStore, vaultStore } = useVaultStore();
+  const { vaultStore } = useVaultStore();
   console.log(vaultStore);
+  const chosenVault: any = vaultStore;
+  const [progressValues, setProgressValues] = useState<any[]>([]);
+  const [loading, setLoading] = useState(true);
 
   const computeProgressBar = (totalDebt: any, collateralValue: any) => {
     // return ((totalDebt / (totalDebt * 1.1)) * 100).toFixed(2);
@@ -39,6 +20,7 @@ const Index = () => {
     console.log("collateralValue", collateralValue);
     console.log(formatUnits(totalDebt, 18));
     console.log(formatUnits(collateralValue, 18));
+
     const ratio =
       Number(formatUnits(totalDebt, 18)) /
       Number(formatUnits(collateralValue, 18));
@@ -46,6 +28,58 @@ const Index = () => {
     console.log("ratio", (ratio * 100).toFixed(2));
     return (ratio * 100).toFixed(2);
   };
+
+  function removeLast18Digits(num: number) {
+    // Convert the number to a string
+    const str = num.toString();
+
+    // Remove the last 18 characters using slice()
+    const resultStr = str.slice(0, -18);
+
+    // Convert the resulting string back to a number
+    const resultNum = Number(resultStr);
+
+    return resultNum;
+  }
+
+  useEffect(() => {
+    if (chosenVault[5] != undefined) {
+      const totalCollateralValue = removeLast18Digits(
+        fromHex(chosenVault[5][2]._hex, "number")
+      );
+
+      const totalDebt = formatEther(chosenVault[5][0]);
+
+      const totalLiquidationValue = Number(totalDebt) * 1.1;
+
+      const borrowLimit = totalCollateralValue - totalCollateralValue * 0.15;
+
+      setProgressValues([
+        {
+          title: "Debt Minted",
+          value: totalDebt,
+          currency: "sEURO",
+        },
+        {
+          title: "Vault Collateral Value",
+          value: totalCollateralValue,
+          currency: "sEURO",
+        },
+        {
+          title: "Vault Collateral Value Liquidation Trigger",
+          value: totalLiquidationValue,
+          currency: "sEURO",
+        },
+        {
+          title: "You can borrow up to:",
+          value: borrowLimit,
+          currency: "sEURO",
+        },
+      ]);
+
+      setLoading(false);
+    }
+  }, [chosenVault]);
 
   return (
     <Box
@@ -74,29 +108,33 @@ const Index = () => {
             alignItems: { xs: "center", sm: "flex-start" },
           }}
         >
-          {dummyData.map((item, index) => (
-            <Box
-              sx={{
-                marginBottom: "10px",
-                marginRight: { xs: "1.5rem", sm: "0" },
-                width: "auto",
-              }}
-              key={index}
-            >
-              <Typography variant="body2">{item.title}</Typography>
-              <Typography
-                variant="body1"
+          {loading ? (
+            <Typography variant="body2">Loading...</Typography>
+          ) : (
+            progressValues.map((item, index) => (
+              <Box
                 sx={{
-                  fontWeight: "bold",
-                  fontSize: "1.5rem",
-                  color: "#fff",
+                  marginBottom: "25px",
+                  marginRight: { xs: "1.5rem", sm: "0" },
+                  width: "auto",
                 }}
+                key={index}
               >
-                {item.value}
-              </Typography>
-              <Typography variant="body2">{item.currency}</Typography>
-            </Box>
-          ))}
+                <Typography variant="body2">{item.title}</Typography>
+                <Typography
+                  variant="body1"
+                  sx={{
+                    fontWeight: "bold",
+                    fontSize: "1.5rem",
+                    color: "#fff",
+                  }}
+                >
+                  {item.value}
+                </Typography>
+                <Typography variant="body2">{item.currency}</Typography>
+              </Box>
+            ))
+          )}
         </Box>
         <Box
           sx={{
@@ -107,12 +145,16 @@ const Index = () => {
           <FullChart />
         </Box>
       </Box>
-      <ProgressBar
-        progressValue={computeProgressBar(
-          Number(ethers.BigNumber.from(vaultStore[5][0])),
-          Number(ethers.BigNumber.from(vaultStore[5][2]))
-        )}
-      />
+      {loading ? (
+        <Typography variant="body2">Loading...</Typography>
+      ) : (
+        <ProgressBar
+          progressValue={computeProgressBar(
+            Number(ethers.BigNumber.from(chosenVault[5][0])),
+            Number(ethers.BigNumber.from(chosenVault[5][2]))
+          )}
+        />
+      )}
     </Box>
   );
 };
