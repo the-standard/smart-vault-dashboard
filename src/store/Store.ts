ba794d94b5f65fa30e318da2882bf8d5bdd4db44
muratcan-yuksel <muratcanyukselpro@gmail.com>
2023-06-20 08:49:19 +0300

    add deposit lottie

--- src/store/Store.ts
+++ src/store/Store.ts
@@ -200,6 +200,7 @@ interface CirccularProgressState {
 export const useCircularProgressStore = create<CirccularProgressState>(
   (set) => ({
     circularProgress: false,
+    //1 for withdraw, 2 for deposit, 3 for new vault
     progressType: 1,
     getCircularProgress: (circularProgress) => set({ circularProgress }),
     getProgressType: (progressType: number) => set({ progressType }),
