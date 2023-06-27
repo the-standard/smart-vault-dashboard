80ec3d29876d6d71c69f2becdf76d0704ee059d5
muratcan-yuksel <muratcanyukselpro@gmail.com>
2023-06-27 12:10:54 +0300

    deployment trigger

--- src/components/collateral/actions/Deposit.tsx
+++ src/components/collateral/actions/Deposit.tsx
@@ -1,5 +1,5 @@
 import { Box, Modal, Typography } from "@mui/material";
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
 import QRCode from "react-qr-code";
 import {
   useVaultAddressStore,
--- src/utils/createClientUtil.ts
+++ src/utils/createClientUtil.ts
@@ -1,6 +1,4 @@
 import { createWalletClient, custom } from "viem";
-import { sepolia, arbitrumGoerli, mainnet } from "viem/chains";
-import { useChainIdStore } from "../store/Store";
 
 const createClientUtil = createWalletClient({
   transport: custom(window.ethereum),
