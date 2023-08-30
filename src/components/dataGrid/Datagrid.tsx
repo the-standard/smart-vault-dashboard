2b533519d4b93f47ea678efef3fd16946b20cdcf
muratcan-yuksel <muratcanyukselpro@gmail.com>
2023-08-30 04:36:59 +0300

    add black bg to modal sides

--- src/components/dataGrid/Datagrid.tsx
+++ src/components/dataGrid/Datagrid.tsx
@@ -490,10 +490,11 @@ const DataGridComponent: React.FC<DataGridComponentProps> = ({ vaults }) => {
                 md: "40%",
               },
               background:
-                "linear-gradient(110.28deg, rgba(26, 26, 26, 0.156) 0.2%, rgba(0, 0, 0, 0.6) 101.11%)",
+                "linear-gradient(110.28deg, rgba(10, 10, 10, 0.8) 0.8%, rgba(0, 0, 0, 0.9) 101.11%)",
               borderRadius: "10px",
               boxShadow: "0 4px 30px rgba(0, 0, 0, 0.1)",
 
+              borderRadius: "10px",
               border: "1px solid rgba(255, 255, 255, 0.3)",
 
               p: 4,
