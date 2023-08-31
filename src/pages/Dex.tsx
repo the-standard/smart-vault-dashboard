e94e426594d57b5d28606179f6456a456feba09d
Zak <zmooney@hotmail.co.uk>
2023-08-31 13:26:51 +0100

    feat: replaced pages cards to use Card component

--- src/pages/Dex.tsx
+++ src/pages/Dex.tsx
@@ -2,26 +2,20 @@ import { Box } from "@mui/material";
 import Exchange from "./Exchange";
 import { arbitrum } from "wagmi/chains";
 
+import Card from "../components/Card";
+
 const Dex = () => {
   return (
-    <Box
+    <Card
       sx={{
-        color: "#8E9BAE",
-        margin: { xs: "0", sm: "3% 12%" },
+        margin: { xs: "3% 3%", sm: "3% 12%" },
         padding: "5%",
-        // marginTop: "50px",
-        background:
-          "linear-gradient(110.28deg, rgba(26, 26, 26, 0.156) 0.2%, rgba(0, 0, 0, 0.6) 101.11%)",
-        border: "1px solid rgba(52, 52, 52, 0.3)",
-        boxShadow: "0px 30px 40px rgba(0, 0, 0, 0.3)",
-        borderRadius: "10px",
         minHeight: "100vh",
         height: "100%",
-        backdropFilter: "blur(13.9px)",
       }}
     >
       <Exchange toChain={arbitrum.id} toToken='0xf5A27E55C748bCDdBfeA5477CB9Ae924f0f7fd2e' />
-    </Box>
+    </Card>
   );
 };
 
