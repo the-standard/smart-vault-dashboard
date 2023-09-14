92193d0178c856c0532fa1e193ce38baae74659e
Zak <zmooney@hotmail.co.uk>
2023-09-14 15:34:12 +0100

    feat: restyled text on yeild page

--- src/pages/Yield.tsx
+++ src/pages/Yield.tsx
@@ -35,11 +35,11 @@ const Yield = () => {
       <Card
         sx={{
           margin: {
-            xs: "0% 4%",
+            xs: "3% 4%",
             sm: "3% 6%",
             md: "3% 12%",
           },      
-          padding: "2%",
+          padding: "1.5rem",
         }}
         ref={rectangleRef}
       >
@@ -57,8 +57,17 @@ const Yield = () => {
               width: "100%",
             }}
           >
-            <Typography variant="h4" sx={{ color: "#fff" }}>
-              Earn a large yield with your EUROs!
+            <Typography
+              variant="h4" 
+              sx={{
+                color: "#fff",
+                fontSize: {
+                  xs: "1.8rem",
+                  md: "2.125rem"
+                }
+              }}
+            >
+              Earn Large Yields With Your EUROs
             </Typography>
             <img src={yields} alt="yields" width="auto" height="130px" />
           </Box>
@@ -73,18 +82,42 @@ const Yield = () => {
                 fontWeight: "bold",
                 color: "#fff",
                 margin: "1rem 0",
+                fontSize: {
+                  xs: "1.5rem",
+                  md: "2.125rem"
+                }
               }}
               variant="h4"
             >
-              Unleash the Magic of Crypto with Camelot! 🌟{" "}
+              Unleash the Magic of Crypto with Camelot 🌟
             </Typography>
-            <Typography variant="h6" sx={{ color: "#fff", marginBottom: "1rem" }}>
+            <Typography
+              sx={{
+                marginBottom: "1rem",
+                fontSize: {
+                  xs: "1rem",
+                  md: "1.2rem",
+                },
+                opacity: "0.9",
+                fontWeight: "300",
+              }}
+            >
               Boost your EUROs by placing them into liquidity pools. By doing
               this, you can own a slice of the biggest decentralized exchange on
               Arbitrum, where every trade funnels rewards straight into your
               crypto wallet using the GRAIL token.
             </Typography>
-            <Typography variant="h6" sx={{ color: "#fff", marginBottom: "1rem" }}>
+            <Typography
+              sx={{
+                marginBottom: "1rem",
+                fontSize: {
+                  xs: "1rem",
+                  md: "1.2rem",
+                },
+                opacity: "0.9",
+                fontWeight: "300",
+              }}            
+            >
               The Standard DAO and Camelot DEX, the hottest exchange on Arbitrum,
               are rolling out the red carpet for YOU, offering a dazzling yield
               PLUS trading fees! Ready to grow your assets? Here's how:
@@ -97,11 +130,11 @@ const Yield = () => {
       <Card
         sx={{
           margin: {
-            xs: "0% 4%",
+            xs: "3% 4%",
             sm: "3% 6%",
             md: "3% 12%",
           },      
-          padding: "2%",
+          padding: "1.5rem",
         }}
       >
         <Box
@@ -120,10 +153,14 @@ const Yield = () => {
                 fontWeight: "bold",
                 color: "#fff",
                 marginBottom: "1rem",
+                fontSize: {
+                  xs: "1.5rem",
+                  md: "2.125rem"
+                }
               }}
-              variant="h6"
+              variant="h4"
             >
-              STEP ONE: Gather Your Treasures! 🪙
+              Step One: Gather Your Treasures 🪙
             </Typography>
           </Box>
         </Box>
@@ -139,6 +176,7 @@ const Yield = () => {
 
           <Box
             sx={{
+              width: "100%",
               display: "flex",
               flexDirection: {
                 xs: "column",
@@ -158,11 +196,31 @@ const Yield = () => {
                 }
               }}
             >
-              <Typography variant="h6" sx={{ color: "#fff", marginBottom: "1rem" }}>
+              <Typography
+                sx={{
+                  marginBottom: "1rem",
+                  fontSize: {
+                    xs: "1rem",
+                    md: "1.2rem",
+                  },
+                  opacity: "0.9",
+                  fontWeight: "300",
+                }}
+              >
                 Need EUROs? Lock assets with The Standard. Borrow EUROs at a 0%
                 interest rate. Magic, right?
               </Typography>
-              <Typography variant="h6" sx={{ color: "#fff", marginBottom: "1rem" }}>
+              <Typography
+                sx={{
+                  marginBottom: "1rem",
+                  fontSize: {
+                    xs: "1rem",
+                    md: "1.2rem",
+                  },
+                  opacity: "0.9",
+                  fontWeight: "300",
+                }}
+              >
                 For USDC.e, swap any asset using our easy-breezy widget.The cool
                 thing with our widget is you can swap any token on any chain over to
                 USDC.e on Arbitrum!
@@ -191,11 +249,11 @@ const Yield = () => {
       <Card
         sx={{
           margin: {
-            xs: "0% 4%",
+            xs: "3% 4%",
             sm: "3% 6%",
             md: "3% 12%",
           },      
-          padding: "2%",
+          padding: "1.5rem",
         }}
       >
         <Box
@@ -214,10 +272,14 @@ const Yield = () => {
                 fontWeight: "bold",
                 color: "#fff",
                 marginBottom: "1rem",
+                fontSize: {
+                  xs: "1.5rem",
+                  md: "2.125rem"
+                }
               }}
-              variant="h6"
+              variant="h4"
             >
-              STEP TWO: Dive into Camelot&apos;s Pools! 🌊{" "}
+              Step Two: Dive into Camelot&apos;s Pools 🌊
             </Typography>
           </Box>
         </Box>
@@ -244,10 +306,30 @@ const Yield = () => {
               },
             }}
           >
-            <Typography variant="h6" sx={{ color: "#fff", marginBottom: "1rem" }}>
+            <Typography
+              sx={{
+                marginBottom: "1rem",
+                fontSize: {
+                  xs: "1rem",
+                  md: "1.2rem",
+                },
+                opacity: "0.9",
+                fontWeight: "300",
+              }}
+            >
               V3 awaits you (already set for your convenience).
-            </Typography>{" "}
-            <Typography variant="h6" sx={{ color: "#fff", marginBottom: "1rem" }}>
+            </Typography>
+            <Typography
+              sx={{
+                marginBottom: "1rem",
+                fontSize: {
+                  xs: "1rem",
+                  md: "1.2rem",
+                },
+                opacity: "0.9",
+                fontWeight: "300",
+              }}
+            >
               Choose EUROs and USDC.e from the treasure trove of pairs.
             </Typography>
           </Box>
@@ -309,7 +391,17 @@ const Yield = () => {
               },
             }}
           >
-            <Typography variant="h6" sx={{ color: "#fff", marginBottom: "1rem" }}>
+            <Typography
+              sx={{
+                marginBottom: "1rem",
+                fontSize: {
+                  xs: "1rem",
+                  md: "1.2rem",
+                },
+                opacity: "0.9",
+                fontWeight: "300",
+              }}
+            >
               Set "AUTO" and let GAMMA, the wise liquidity wizard, optimize your
               rewards without you lifting a finger.{" "}
             </Typography>{" "}
@@ -354,11 +446,11 @@ const Yield = () => {
       <Card
         sx={{
           margin: {
-            xs: "0% 4%",
+            xs: "3% 4%",
             sm: "3% 6%",
             md: "3% 12%",
           },      
-          padding: "2%",
+          padding: "1.5rem",
         }}
       >
         <Box
@@ -377,10 +469,14 @@ const Yield = () => {
                 fontWeight: "bold",
                 color: "#fff",
                 marginBottom: "1rem",
+                fontSize: {
+                  xs: "1.5rem",
+                  md: "2.125rem"
+                }
               }}
-              variant="h6"
+              variant="h4"
             >
-              STEP THREE: Make It Rain Rewards! 🌧️💰{" "}
+              Step Three: Make It Rain Rewards 🌧️💰
             </Typography>
           </Box>
         </Box>
@@ -406,17 +502,47 @@ const Yield = () => {
               },
             }}
           >
-            <Typography variant="h6" sx={{ color: "#fff", marginBottom: "1rem" }}>
+            <Typography
+              sx={{
+                marginBottom: "1rem",
+                fontSize: {
+                  xs: "1rem",
+                  md: "1.2rem",
+                },
+                opacity: "0.9",
+                fontWeight: "300",
+              }}
+            >
               Decide how much EUROs or USDC you wish to pour into the liquidity
-              cauldron.{" "}
-            </Typography>{" "}
-            <Typography variant="h6" sx={{ color: "#fff", marginBottom: "1rem" }}>
-              Grant your royal approval and seal the deal with "Create Position".{" "}
-            </Typography>{" "}
-            <Typography variant="h6" sx={{ color: "#fff", marginBottom: "1rem" }}>
+              cauldron.
+            </Typography>
+            <Typography
+              sx={{
+                marginBottom: "1rem",
+                fontSize: {
+                  xs: "1rem",
+                  md: "1.2rem",
+                },
+                opacity: "0.9",
+                fontWeight: "300",
+              }}
+            >
+              Grant your royal approval and seal the deal with "Create Position".
+            </Typography>
+            <Typography
+              sx={{
+                marginBottom: "1rem",
+                fontSize: {
+                  xs: "1rem",
+                  md: "1.2rem",
+                },
+                opacity: "0.9",
+                fontWeight: "300",
+              }}
+            >
               🌈 Voilà! Revel in the continuous shower of trading fees and a
-              strong yield paid in the majestic GRAIL tokens.{" "}
-            </Typography>{" "}
+              strong yield paid in the majestic GRAIL tokens.
+            </Typography>
           </Box>
 
         </Box>
@@ -427,11 +553,11 @@ const Yield = () => {
       <Card
         sx={{
           margin: {
-            xs: "0% 4%",
+            xs: "3% 4%",
             sm: "3% 6%",
             md: "3% 12%",
           },      
-          padding: "2%",
+          padding: "1.5rem",
         }}
       >
         <Box
@@ -450,10 +576,14 @@ const Yield = () => {
                 fontWeight: "bold",
                 color: "#fff",
                 marginBottom: "1rem",
+                fontSize: {
+                  xs: "1.5rem",
+                  md: "2.125rem"
+                }
               }}
-              variant="h6"
+              variant="h4"
             >
-              STEP FOUR: Let&apos;s up the reward by 5%
+              Step Four: Let&apos;s up the reward by 5% ✨
             </Typography>
           </Box>
         </Box>
@@ -480,13 +610,33 @@ const Yield = () => {
               },
             }}
           >
-            <Typography variant="h6" sx={{ color: "#fff", marginBottom: "1rem" }}>
+            <Typography
+              sx={{
+                marginBottom: "1rem",
+                fontSize: {
+                  xs: "1rem",
+                  md: "1.2rem",
+                },
+                opacity: "0.9",
+                fontWeight: "300",
+              }}
+            >
               But why stop there? Supercharge your yield even higher by placing
               your spNFT liquidity tokens into the NITRO pool. This will grant you
               the extra APR in TST tokens. These tokens are what will give you a
               cut of all fees generated on TheStandard Protocol.{" "}
             </Typography>
-            <Typography variant="h6" sx={{ color: "#fff", marginBottom: "1rem" }}>
+            <Typography
+              sx={{
+                marginBottom: "1rem",
+                fontSize: {
+                  xs: "1rem",
+                  md: "1.2rem",
+                },
+                opacity: "0.9",
+                fontWeight: "300",
+              }}
+            >
               Join us, and let's make crypto magic together on{" "}
               <a
                 style={{
